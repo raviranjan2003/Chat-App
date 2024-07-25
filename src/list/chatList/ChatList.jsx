b16a93b000e2059deda94b6de14dabcb96382a68
raviranjan2003 <2140131@sliet.ac.in>
2024-07-25 11:05:50 +0530

    searchBar style updated

---
+++ src/list/chatList/ChatList.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import "./chatList.css";
+
+const ChatList = () => {
+  const [addMode, setAddMode] = useState(false);
+
+  return (
+    <div className='chatList'>
+      <div className='search'>
+        <div className='searchBar'>
+          <img src='/search.png' alt='img'/>
+          <input type="text" placeholder='Search'/>
+        </div>
+        <img 
+          src={addMode ? "./minus.png" : "./plus.png"} 
+          alt="img" 
+          className='add'
+          onClick={() => setAddMode((prev) => !prev)}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default ChatList;
