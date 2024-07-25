41695fbe551a6d980ba303f750f7ff4057df04a0
raviranjan2003 <2140131@sliet.ac.in>
2024-07-25 11:35:28 +0530

    chatList style updated

--- src/list/chatList/ChatList.jsx
+++ src/list/chatList/ChatList.jsx
@@ -18,6 +18,41 @@ const ChatList = () => {
           onClick={() => setAddMode((prev) => !prev)}
         />
       </div>
+      <div className='item'>
+        <img src="./avatar.png" alt="" />
+        <div className='texts'>
+          <span> Ravi Ranjan</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className='item'>
+        <img src="./avatar.png" alt="" />
+        <div className='texts'>
+          <span> Ravi Ranjan</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className='item'>
+        <img src="./avatar.png" alt="" />
+        <div className='texts'>
+          <span> Ravi Ranjan</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className='item'>
+        <img src="./avatar.png" alt="" />
+        <div className='texts'>
+          <span> Ravi Ranjan</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className='item'>
+        <img src="./avatar.png" alt="" />
+        <div className='texts'>
+          <span> Ravi Ranjan</span>
+          <p>Hello</p>
+        </div>
+      </div>
     </div>
   )
 }
