6933497392c362c06f950316202ce271a03d53bd
raviranjan2003 <2140131@sliet.ac.in>
2024-07-25 16:40:01 +0530

    style:userDetail

---
+++ src/detail/Detail.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import "./detail.css";
+
+const Detail = () => {
+  return (
+    <div className="detail">
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>Ravi Ranjan</h2>
+        <p>Lorem ipsum dolor sit amet consecte.</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png" alt="img" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy & Help</span>
+            <img src="./arrowUp.png" alt="img" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Photos</span>
+            <img src="./arrowDown.png" alt="img" />
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="./avatar.png" alt="img" />
+                <span>photo_2024.png</span>
+              </div>
+              <img src="./download.png" alt="img" className='icon' />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="./avatar.png" alt="img" />
+                <span>photo_2024.png</span>
+              </div>
+              <img src="./download.png" alt="img" className='icon' />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="./avatar.png" alt="img" />
+                <span>photo_2024.png</span>
+              </div>
+              <img src="./download.png" alt="img" className='icon' />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="./avatar.png" alt="img" />
+                <span>photo_2024.png</span>
+              </div>
+              <img src="./download.png" alt="img" className='icon' />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="./avatar.png" alt="img" />
+                <span>photo_2024.png</span>
+              </div>
+              <img src="./download.png" alt="img" className='icon' />
+            </div>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <img src="./arrowUp.png" alt="img" />
+          </div>
+        </div>
+        <button>Block</button>
+      </div>
+    </div>
+  )
+}
+
+export default Detail
