77d437f2686e53247144e69ba505087ada80328c
raviranjan2003 <2140131@sliet.ac.in>
2024-07-26 12:44:11 +0530

    feat:signin and signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import Chat from "./chat/Chat"
 import Detail from "./detail/Detail"
 import List from "./list/List"
+import Login from "./login/Login";
 
 const App = () => {
+  const user = false;
+
   return (
     <div className='container'>
-      <List></List>
-      <Chat></Chat>
-      <Detail></Detail>
+      {user ? 
+        <>
+          <List></List>
+          <Chat></Chat>
+          <Detail></Detail>
+        </> :
+        <Login />
+      }
     </div>
   )
 }
