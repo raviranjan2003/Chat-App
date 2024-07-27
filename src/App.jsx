160a24823022b781f8b078925efa4f9c9911cd55
raviranjan2003 <2140131@sliet.ac.in>
2024-07-27 10:51:49 +0530

    UI integrated with zustand

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
+import { useEffect, useState } from "react";
 import Chat from "./chat/Chat"
 import Detail from "./detail/Detail"
 import List from "./list/List"
 import Login from "./login/Login";
 import Notification from "./notification/Notification";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./lib/firebase";
+import useUserStore from "./lib/store";
 
 const App = () => {
-  const user = true;
+
+  const { currentUser, isLoading, fetchUserInfo } = useUserStore();
+
+  useEffect(()=>{
+    const unSub = onAuthStateChanged(auth, user =>{
+      // console.log(user);
+      fetchUserInfo(user.uid);
+      if(user) {
+        setUser(true);
+      }
+    })
+
+    return () => {
+      unSub();
+    }
+  }, [fetchUserInfo]);
+
+  console.log(currentUser);
+
+  if(isLoading) return <div className="loading">Loading...</div>
 
   return (
     <div className='container'>
-      {user ? 
+      {currentUser ? 
         <>
           <List></List>
           <Chat></Chat>
