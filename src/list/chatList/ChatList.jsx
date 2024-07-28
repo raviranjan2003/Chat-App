21df53158c8bbbf58029ce65d400ce04731527d6
raviranjan2003 <2140131@sliet.ac.in>
2024-07-28 11:54:33 +0530

    style: isSeen is now dynamic

--- src/list/chatList/ChatList.jsx
+++ src/list/chatList/ChatList.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import "./chatList.css";
 import Adduser from './adduser/Adduser';
-import { doc, getDoc, onSnapshot } from 'firebase/firestore';
+import { doc, getDoc, onSnapshot, updateDoc } from 'firebase/firestore';
 import { db } from '../../lib/firebase';
 import useUserStore from '../../lib/store';
 import useChatStore from '../../lib/chatStore';
@@ -47,7 +47,26 @@ const ChatList = () => {
   },[currentUser.id]);
 
   const handleSelect = async (chat) => {
-    changeChat(chat.chatId, chat.user);
+
+    const userChats = chats.map((item) => {
+      const { user, ...rest } = item;
+      return rest;
+    })
+    const chatIndex = userChats.findIndex(item => item.chatId === chat.chatId);
+
+    userChats[chatIndex].isSeen = true;
+
+    const userChatsRef = doc(db, "userchats", currentUser.id);
+
+    try {
+      await updateDoc(userChatsRef, {
+        chats: userChats
+      })
+      // Updating store for selected user
+      changeChat(chat.chatId, chat.user);
+    } catch (error) {
+      console.log(error)
+    }
   }
   return (
     <div className='chatList'>
@@ -65,7 +84,14 @@ const ChatList = () => {
       </div>
       {
         chats.map(chat => (
-      <div className='item' key={chat.chatId} onClick={()=>handleSelect(chat)}>
+      <div 
+        className='item' 
+        key={chat.chatId} 
+        onClick={()=>handleSelect(chat)}
+        style={{ 
+          backgroundColor : chat?.isSeen ? "transparent" : "#5183fe"
+        }}
+      > 
         <img src={chat.user.avatar || "./avatar.png"} alt="" />
         <div className='texts'>
           <span>{chat.user.username}</span>
